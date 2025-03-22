a950f1deb8f1a6ca97f21ff4cb85a5e723debb4f
bot <info@devopsinsiders.com>
2025-03-22 10:44:25 +0530

    Updated Notes

--- CodeSamples/Terraform/Class4/nimbu.tf
+++ CodeSamples/Terraform/Class4/nimbu.tf
@@ -3,19 +3,6 @@ provider "azurerm" {
   subscription_id = "1075ec7a-b17a-4f37-bf3f-9d68c4506dc1"
 }
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "rg-nimbu"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = "rg-nimbu"
-  location                 = "Central India"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
-
 terraform {
   required_providers {
     azurerm = {
@@ -24,3 +11,10 @@ terraform {
     }
   }
 }
+
+resource "azurerm_resource_group" "dhondhu-block" {
+  name     = "dhondhu-rg"
+  location = "West Europe"
+}
+
+
