6e952a5f53ebeff4dc42c4fb94fd95ba9ac2dfc3
Rishabh Raina <rishabhraina8@gmail.com>
2025-10-01 20:46:05 +0530

    code commit: configuration updated for variables.tf in aws vpc module.

--- CodeSamples/TodoApp/InfraAutomation/modules/aws_vpc/variables.tf
+++ CodeSamples/TodoApp/InfraAutomation/modules/aws_vpc/variables.tf
@@ -25,3 +25,13 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+}
